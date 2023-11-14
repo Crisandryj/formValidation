4514fb28dce063921fe70e80d4ac0320556ef405
crisandryj <crisandryj@gmail.com>
2023-11-14 14:43:56 -0500

    Feat: Vaildate email and zip code

--- myscripts.js
+++ myscripts.js
@@ -1,47 +1,58 @@
-// There are many ways to pick a DOM node; here we get the form itself and the email
-// input box, as well as the span element into which we will place the error message.
-const form = document.querySelector("form");
-const email = document.getElementById("mail");
-const emailError = document.querySelector("#mail + span.error");
+const email = document.getElementById("email");
+const zip = document.getElementById("zipcode");
+const country = document.getElementById("country");
+const error = document.getElementById("errormessage");
 
-email.addEventListener("input", (event) => {
-  // Each time the user types something, we check if the
-  // form fields are valid.
-
+email.addEventListener("input", () => {
   if (email.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    emailError.textContent = ""; // Reset the content of the message
-    emailError.className = "error"; // Reset the visual state of the message
+    error.textContent = "";
+    error.className = "error";
   } else {
-    // If there is still an error, show the correct error
-    showError();
+    showEmailError();
   }
 });
 
-form.addEventListener("submit", (event) => {
-  // if the email field is valid, we let the form submit
-  if (!email.validity.valid) {
-    // If it isn't, we display an appropriate error message
-    showError();
-    // Then we prevent the form from being sent by canceling the event
-    event.preventDefault();
-  }
-});
-
-function showError() {
+function showEmailError() {
   if (email.validity.valueMissing) {
     // If the field is empty,
     // display the following error message.
-    emailError.textContent = "You need to enter an email address.";
+    error.textContent = "You need to enter an email address.";
   } else if (email.validity.typeMismatch) {
     // If the field doesn't contain an email address,
     // display the following error message.
-    emailError.textContent = "Entered value needs to be an email address.";
+    error.textContent = "Entered value needs to be an email address.";
   } else if (email.validity.tooShort) {
     // If the data is too short,
     // display the following error message.
-    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    error.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  // Set the styling appropriately
+  emailError.className = "error active";
+}
+
+zip.addEventListener("input", () => {
+  if (zip.validity.valid) {
+    error.textContent = "";
+    error.className = "error";
+  } else {
+    showZipError();
+  }
+});
+
+function showZipError() {
+  if (zip.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    error.textContent = "You need to enter an email address.";
+  } else if (zip.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    error.textContent = "Entered value needs to be an email address.";
+  } else if (zip.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    error.textContent = `Zip should be at least ${zip.minLength} numbers; you entered ${zip.value.length}.`;
   }
 
   // Set the styling appropriately
