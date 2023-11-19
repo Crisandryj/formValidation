4081268a127959522dfdefbd831c5ebd41ab68e7
crisandryj <crisandryj@gmail.com>
2023-11-19 09:38:16 -0500

    Feat: Create adjusted index to wrap around array

--- practice.rb
+++ practice.rb
@@ -1,6 +1,14 @@
 def caesar_cipher(string, _num)
   arry = string.split('')
   puts ('a'..'z').to_a
+
+  puts
 end
 
-caesar_cipher('gp', 5)
+# caesar_cipher('gp', 5)
+
+def adjusted_index(index, offset)
+  (index + offset) % 26
+end
+
+puts adjusted_index(25, 2)
